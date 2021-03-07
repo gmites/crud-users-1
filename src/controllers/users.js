302998ee15caad7865a7e6a99cb1c0b7921efa8f
DESKTOP-HFE9SM7\khaleesi <grace.mites@gmail.com>
2021-03-07 21:49:12 +0100

    Update user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -25,7 +25,7 @@ const getAllUsers = async(req, res)=>{
 
 const getUser = async(req, res)=>{
     const dni = req.params.dni
-    const data = await userModel.find({ dni: dni })
+    const data = await userModel.findOne({ dni: dni })
     const response = {
         status: 'Success',
         data: {
@@ -35,8 +35,39 @@ const getUser = async(req, res)=>{
     res.json(response) 
 }
 
-const updateUser = (req, res)=>{
-    res.send('Update User') 
+const updateUser = async(req,res)=>{
+   const dni = req.params.dni
+    const newData = req.body
+    const user = await userModel.findOne({ dni: dni })
+    console.log('userdelFind', user)
+    user.name = newData.name
+    user.age = newData.age
+    user.dni = newData.dni
+    const data = await user.save()
+    const response = {
+        status: 'Success',
+        data: {
+            user:[data]
+        }
+    }
+    res.json(response)
+    
+    /*const id = req.params.dni
+    const newData = req.body
+    const user = await userModel.findById(id)
+    console.log('userdelFind', user)
+    user.name = newData.name
+    user.age = newData.age
+    user.dni = newData.dni
+    console.log('luegoMod', user)
+    const data = await user.save()
+    const response = {
+        status: 'Success',
+        data: {
+            user:[data]
+        }
+    }
+    res.json(response) */
 }
 
 const deleteUser = (req, res)=>{
